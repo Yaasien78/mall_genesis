f262a5507ea5e6bfbef10f287817360696725839
Yaasien78 <141853765+Yaasien78@users.noreply.github.com>
2026-06-18 17:09:47 +0700

    Add appId to Pi initialization

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
       await window.Pi.init({
         version: "2.0",
         sandbox: true
+        appId:"vz83ocyd1n24qre7bhayswur7gop1xzydavhmlvc5cimggyuad0wffenydytchkn"
       })
 
       const scopes = ['username'];
